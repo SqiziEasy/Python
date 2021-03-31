b72e5090b90eed5a10a3d0a574a9da934550056b
sqizi <danila.goro@yandex.ru>
2021-04-01 00:28:07 +0300

    hierarchy

---
+++ key.py
@@ -0,0 +1,20 @@
+objects = [{
+    "math": [
+    {
+        "task_number": [
+        {
+            "3": [
+            {
+                "variants_of_task": [
+                {
+                    "difficult": "1",
+                    "question": "текст задания",
+                    "answer": "122"
+                }]
+            }]
+        }]
+    }]
+}]
+
+print(objects[0])
+#["task_number"]["3"]["variants_of_task"])
