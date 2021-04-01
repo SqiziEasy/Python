7bc5e39c4819d2a33aaf3aa3390a91b27dd95981
sqizi <danila.goro@yandex.ru>
2021-04-01 23:36:02 +0300

    users

---
+++ users.py
@@ -0,0 +1,16 @@
+users = {
+    "id1": {
+        "name": "имя",
+        "current_object": "math",
+        "current_task_number": "3",
+        "current_variant_of_task": 5
+        "completed_question": 1, 3, 7
+    },
+    "id2": {
+        "name": "имя",
+        "current_object": "math",
+        "current_task_number": "3",
+        "current_variant_of_task": 2,
+        "completed_question": 1, 3, 7
+    }
+}
