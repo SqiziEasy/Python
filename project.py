3e395a17523580ea30ae119ccbed3f3f4b1f52ef
sqizi <danila.goro@yandex.ru>
2021-04-07 16:13:19 +0300

    idk

--- project.py
+++ project.py
@@ -2,221 +2,216 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, InlineQueryHandler, CallbackQueryHandler, CommandHandler, MessageHandler, Filters
 import requests
 import math
+import random
 
 count_btn_inline = 3
 
-objects = [{
-    "math": [
-    {  
-        "1": [
-        {
-            "variants_of_task": [
-            {
-                "difficult": "1",
-                "question": "текст задания",
-                "answer": "ответ"
-            }, 
-            {
-                "difficult": "2",
-                "question": "текст задания",
-                "answer": "ответ"
-            },
-            {
-                "difficult": "3",
-                "question": "текст задания",
-                "answer": "ответ"
-            }]
-        }]
+objects = {
+    "math": {
+        "name": "Математика",
+        "questions": [
+            [
+                {
+                    "difficult": "1",
+                    "question": "текст задания",
+                    "answer": "ответ"
+                }, 
+                {
+                    "difficult": "2",
+                    "question": "текст задания",
+                    "answer": "ответ"
+                },
+                {
+                    "difficult": "3",
+                    "question": "текст задания",
+                    "answer": "ответ"
+                }
+            ],
+            [
+                {
+                    "difficult": "1",
+                    "question": "текст задания",
+                    "answer": "ответ"
+                }, 
+                {
+                    "difficult": "2",
+                    "question": "текст задания",
+                    "answer": "ответ"
+                },
+                {
+                    "difficult": "3",
+                    "question": "текст задания",
+                    "answer": "ответ"
+                }
+            ],
+            [
+                {
+                    "difficult": "1",
+                    "question": "текст задания",
+                    "answer": "ответ"
+                }, 
+                {
+                    "difficult": "2",
+                    "question": "текст задания",
+                    "answer": "ответ"
+                },
+                {
+                    "difficult": "3",
+                    "question": "текст задания",
+                    "answer": "ответ"
+                }
+            ]
+        ]
     },
-    {
-        "2": [
-        {
-            "variants_of_task": [
-            {
-                "difficult": "1",
-                "question": "текст задания",
-                "answer": "ответ"
-            }, 
-            {
-                "difficult": "2",
-                "question": "текст задания",
-                "answer": "ответ"
-            },
-            {
-                "difficult": "3",
-                "question": "текст задания",
-                "answer": "ответ"
-            }]
-        }]
+    "rus": {
+        "name": "Русский язык",
+        "questions": [
+            [
+                {
+                    "difficult": "1",
+                    "question": "текст задания",
+                    "answer": "ответ"
+                }, 
+                {
+                    "difficult": "2",
+                    "question": "текст задания",
+                    "answer": "ответ"
+                },
+                {
+                    "difficult": "3",
+                    "question": "текст задания",
+                    "answer": "ответ"
+                }
+            ],
+            [
+                {
+                    "difficult": "1",
+                    "question": "текст задания",
+                    "answer": "ответ"
+                }, 
+                {
+                    "difficult": "2",
+                    "question": "текст задания",
+                    "answer": "ответ"
+                },
+                {
+                    "difficult": "3",
+                    "question": "текст задания",
+                    "answer": "ответ"
+                }
+            ],
+            [
+                {
+                    "difficult": "1",
+                    "question": "текст задания",
+                    "answer": "ответ"
+                }, 
+                {
+                    "difficult": "2",
+                    "question": "текст задания",
+                    "answer": "ответ"
+                },
+                {
+                    "difficult": "3",
+                    "question": "текст задания",
+                    "answer": "ответ"
+                }
+            ]
+        ]
     },
-    {
-       "3": [
-        {
-            "variants_of_task": [
-            {
-                "difficult": "1",
-                "question": "текст задания",
-                "answer": "ответ"
-            }, 
-            {
-                "difficult": "2",
-                "question": "текст задания",
-                "answer": "ответ"
-            },
-            {
-                "difficult": "3",
-                "question": "текст задания",
-                "answer": "ответ"
-            }]
-        }] 
-    }]
-},
-{
-    "rus": [
-    {  
-        "1": [
-        {
-            "variants_of_task": [
-            {
-                "difficult": "1",
-                "question": "текст задания",
-                "answer": "ответ"
-            }, 
-            {
-                "difficult": "2",
-                "question": "текст задания",
-                "answer": "ответ"
-            },
-            {
-                "difficult": "3",
-                "question": "текст задания",
-                "answer": "ответ"
-            }]
-        }]
-    },
-    {
-        "2": [
-        {
-            "variants_of_task": [
-            {
-                "difficult": "1",
-                "question": "текст задания",
-                "answer": "ответ"
-            }, 
-            {
-                "difficult": "2",
-                "question": "текст задания",
-                "answer": "ответ"
-            },
-            {
-                "difficult": "3",
-                "question": "текст задания",
-                "answer": "ответ"
-            }]
-        }]
-    },
-    {
-       "3": [
-        {
-            "variants_of_task": [
-            {
-                "difficult": "1",
-                "question": "текст задания",
-                "answer": "ответ"
-            }, 
-            {
-                "difficult": "2",
-                "question": "текст задания",
-                "answer": "ответ"
-            },
-            {
-                "difficult": "3",
-                "question": "текст задания",
-                "answer": "ответ"
-            }]
-        }] 
-    }]
-}]
+}
 
-users = []
+users = {}
 
-def start(update, context):
-    chat_id = update.message.chat_id
-
-    users.append({
-        str(chat_id): {
-        "current_object": "",
-        "current_task_number": "",
-        "current_variant_of_task": "",
-        "completed_question": []
-        }
-    })
-
+def gen_set_obj_keyboard(update, context, chat_id):
     keyboard = [[] for i in range(math.ceil(len(objects) / count_btn_inline))]
 
     for idx, key in enumerate(objects):
-        keyboard[math.floor(idx / count_btn_inline)].append(InlineKeyboardButton(users[0][str(chat_id)]["current_object"], callback_data=('set_obj ' + str(key))))
+        keyboard[math.floor(idx / count_btn_inline)].append(InlineKeyboardButton(objects[key]['name'], callback_data=('set_obj ' + str(key) + ' ' + str(chat_id))))
 
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     context.bot.send_message(chat_id=chat_id, text="Выберите предмет", reply_markup=reply_markup)
 
+def start(update, context):
+    chat_id = update.message.chat_id
 
+    users[chat_id] = {
+        "current_object": "",
+        "current_task_number": "",
+        "completed_question": []
+    }
+
+    gen_set_obj_keyboard(update, context, chat_id)
+
+def go_to_completening_question_keyboard(update, context, chat_id):
+
+    keyboard_cmplt = [[
+        InlineKeyboardButton(" Перейти к решению номера", callback_data=("go_to_completening_question" + str(chat_id)))
+    ]]
+
+    reply_markup = InlineKeyboardMarkup(keyboard_cmplt)
+
+    context.bot.send_message(chat_id=chat_id, text="Перейти к решению задания", reply_markup=reply_markup)
+
+    #keyboard = [[] for i in range(count_btn_inline)]
+    #keyboard[0].append(InlineKeyboardButton("Перейти к решению номера", callback_data=("go_to_completening_question"  + str(chat_id))))
+    #reply_markup = InlineKeyboardMarkup(keyboard)
+    #random.randint(0, objects[key]["questions"].len - 1)
 
 def button(update, context):
     query = update.callback_query
 
     command = query.data.split()
 
+    print(users)
+
+
     if command[0] == 'set_obj':
-        users[0][str(chat_id)]["current_object"] = command[1]
+        print(int(command[2]))
 
-        msg = "Вы выбрали " + users[0][str(chat_id)]["current_object"] + " в качестве предмета для подготовки."  
+        users[int(command[2])]["current_object"] = command[1]
 
-        query.edit_message_text(text=msg)
+        msg = "Вы выбрали " + objects[command[1]]["name"] + " в качестве предмета для подготовки."  
 
-    query.answer()
+        keyboard = [[] for i in range(math.ceil(len(objects[command[1]]['questions']) / count_btn_inline))]
 
+        for idx in range(len(objects[command[1]]['questions'])):
+            keyboard[math.floor(idx / count_btn_inline)].append(InlineKeyboardButton(idx + 1, callback_data=('set_n_of_task ' + str(idx) + ' ' + command[2])))
+
+        keyboard[math.floor(idx / count_btn_inline)].append(InlineKeyboardButton("Выбрать предмет", callback_data=('go_to_set_object ' + command[2])))
+
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        
+        query.edit_message_text(text=msg, reply_markup=reply_markup)
     elif command[0] == 'set_n_of_task':
-        users[0][str(chat_id)]["current_task_number"] = command[1]
+        users[int(command[2])]["current_task_number"] = int(command[1])
 
-        msg = "Вы выбрали " + users[0][str(chat_id)]["current_task_number"] + " в качестве номера для подготовки."  
+        msg = "Вы выбрали " + str(users[int(command[2])]["current_task_number"] + 1) + " в качестве номера для подготовки."  
 
         query.edit_message_text(text=msg)
 
+        go_to_completening_question_keyboard(update, context, command[1])
+    elif command[0] == 'go_to_set_object':
+        gen_set_obj_keyboard(update, context, command[1])
+
+    elif command[0] == "go_to_completening_question":
+        go_to_completening_question_keyboard(update, context, command[1])
+
+        query.edit_message_text(text=msg, reply_markup=reply_markup)
+
+
     query.answer()
-
-
-
-#def echo(update, context):
-#    chat_id = update.message.chat_id
-#    if update.message.text == "Выбери предмет математика":
-#        users[0][str(chat_id)]["current_object"] = "math"
-#        context.bot.send_message(chat_id=update.effective_chat.id, text=users)
-#    elif update.message.text == "Выбери предмет русский":
-#        users[0][str(chat_id)]["current_object"] = "rus"
-#        context.bot.send_message(chat_id=update.effective_chat.id, text=users)
-#    elif update.message.text == "Выбери номер 1":
-#        users[0][str(chat_id)]["current_task_number"] = "1"
-#    elif update.message.text == "Выбери номер 2":
-#       users[0][str(chat_id)]["current_task_number"] = "2"
-#    elif update.message.text == "Выбери номер 3":
-#        users[0][str(chat_id)]["current_task_number"] = "3"
-
-
-
 
 
 def main():
-    updater = Updater('1787533409:AAGiSxbovcVvYE4dElEfnx2dKm01Oz-BCmM', use_context=True)
+    updater = Updater('1787533409:AAHes3McJcf49B5JnbMSIIgQXJxqy8aizkg', use_context=True)
 
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler('start', start))
-
     dp.add_handler(CallbackQueryHandler(button))
-    #dp.add_handler(MessageHandler(Filters.text & (~Filters.command), echo))
-    #dp.add_handler(adding_user_handler)
+
     updater.start_polling()
     updater.idle()
 
